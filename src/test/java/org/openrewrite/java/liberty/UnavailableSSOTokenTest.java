8b6cf025ad46372dfaffd199ca368d155fd2a442
Tim te Beek <tim@moderne.io>
2023-08-15 12:08:44 +0200

    Minimze UnavailableSSOTokenTest

--- src/test/java/org/openrewrite/java/liberty/UnavailableSSOTokenTest.java
+++ src/test/java/org/openrewrite/java/liberty/UnavailableSSOTokenTest.java
@@ -16,116 +16,55 @@
 package org.openrewrite.java.liberty;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.InMemoryExecutionContext;
 import org.openrewrite.config.Environment;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.java;
 
-public class UnavailableSSOTokenTest implements RewriteTest {
+class UnavailableSSOTokenTest implements RewriteTest {
 
-    String ssoTokenNameClass = """
-                                package com.ibm.websphere.security;
-
-                                import javax.servlet.http.Cookie;
-                                
-                                public class WSSecurityHelper {
-                                
-                                
-                                        public static  Cookie getLTPACookieFromSSOToken(){
-                                            return new Cookie();
-                                            
-                                        }
-                                    }         
-                                          
-                               """;
-    String cookieNameClass = """
-                            package javax.servlet.http;
-
-                            public class Cookie {
-
-                            }  
-                            """;
-    
-    
-                            
-    
-    @Test
-    void doesNotModifySSOtokensAlreadyPresent() {
-        rewriteRun(
-            java(cookieNameClass),
-            java(ssoTokenNameClass),
-            java(
-                """
-                    package com.ibm;
-                    import com.ibm.websphere.security.WSSecurityHelper;
-                    com.ibm.websphere.security.web.WebSecurityHelper; 
-                    public class Test{
-                            public void doX() {
-                                Cookie ltpaCookie = WebSecurityHelper.getSSOCookieFromSSOToken();
-                             }
-                    }
-                """
-            )
-        );
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec
+          .parser(JavaParser.fromJavaVersion()
+            .classpathFromResources(new InMemoryExecutionContext(), "websecurity_logout_test"))
+          .recipe(Environment.builder().scanRuntimeClasspath("org.openrewrite.java.liberty").build()
+            .activateRecipes("org.openrewrite.java.liberty.WebSphereUnavailableSSOTokenMethod"));
     }
-    @Test
-    void doesNotModifySSOtokensNotPresent() {
-        rewriteRun(
-            java(cookieNameClass),
-            java(ssoTokenNameClass),
-            java(
-                """
-                    package com.ibm;
-                    com.ibm.websphere.security.web.WebSecurityHelper; 
-                    
-                    public class Test{               
 
-                       int x = 10;
-                       public void  helloworld(){
-                            System.out.println("hello world");
-                       }
-                    }
-                """
-            )
-        );
-    }
 
     @Test
     void updatesLPTACookieToSSO() {
         rewriteRun(
-            spec -> spec.recipe(Environment.builder().scanRuntimeClasspath("org.openrewrite.java.liberty").build().activateRecipes("org.openrewrite.java.liberty.WebSphereUnavailableSSOTokenMethod")),      
-            java(cookieNameClass),
-            java(ssoTokenNameClass),
-            java(
-                """
-                    package com.ibm;
-                    
-                    import javax.servlet.http.Cookie;
-                    import com.ibm.websphere.security.WSSecurityHelper;
+          //language=java
+          java(
+            """                  
+              import com.ibm.websphere.security.WSSecurityHelper;
+                                
+              import javax.servlet.http.Cookie;
 
-                    public class Test{               
-
-                        public void doX() {
-                            Cookie ltpaCookie = WSSecurityHelper.getLTPACookieFromSSOToken();
-                        }
-                    }                   
-                """,
-                """
-                    package com.ibm;
-
-                    import com.ibm.websphere.security.web.WSSecurityHelper;
-                    
-                    import javax.servlet.http.Cookie;
-                    
-                    public class Test{
-                    
-                        public void doX() {
-                            Cookie ltpaCookie = WSSecurityHelper.getSSOCookieFromSSOToken();
-                        }
-                    }
-                """
-            )
+              public class Test {
+                  public void doX() {
+                      Cookie ltpaCookie = WSSecurityHelper.getLTPACookieFromSSOToken();
+                  }
+              }
+              """,
+            """
+              import com.ibm.websphere.security.web.WSSecurityHelper;
+                                
+              import javax.servlet.http.Cookie;
+                                
+              public class Test {
+                  public void doX() {
+                      Cookie ltpaCookie = WSSecurityHelper.getSSOCookieFromSSOToken();
+                  }
+              }
+              """
+          )
         );
-    } 
-    
+    }
+
 }
