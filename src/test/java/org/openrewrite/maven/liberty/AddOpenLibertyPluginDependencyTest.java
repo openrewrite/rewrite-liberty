09fd9058d6371a5c9ef8d6b3aa4ebafce51daa2d
anuram <ranuradh@us.ibm.com>
2024-10-11 11:29:15 -0400

    adding addLibertyMvnPlugin recipe

---
+++ src/test/java/org/openrewrite/maven/liberty/AddOpenLibertyPluginDependencyTest.java
@@ -0,0 +1,64 @@
+package org.openrewrite.maven.liberty;
+
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.Test;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.*;
+import static org.openrewrite.maven.Assertions.pomXml;
+
+public class AddOpenLibertyPluginDependencyTest  implements RewriteTest {
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.parser(JavaParser.fromJavaVersion());
+    }
+
+    @Language("java")
+    private final String sampleClass = """
+         package com.test;
+
+         public class Test {         
+              public static void main(String[] args)
+              {
+                  System.out.println("test");
+               }
+         }     
+      """;
+    @Test
+    void testAddLibertyPlugin() {
+        rewriteRun(
+          spec ->  spec.recipeFromResources("org.openrewrite.maven.liberty.AddOpenLibertyPluginDependency"),
+          mavenProject(
+            "project",
+            srcMainJava(
+              java(sampleClass)
+            ),
+            pomXml(
+              """
+                    <project>
+                        <groupId>com.mycompany.app</groupId>
+                        <artifactId>my-app</artifactId>
+                        <version>1</version>
+                    </project>
+                """,
+              """
+                    <project>
+                        <groupId>com.mycompany.app</groupId>
+                        <artifactId>my-app</artifactId>
+                        <version>1</version>
+                        <dependencies>
+                            <dependency>
+                                <groupId>io.openliberty.tools</groupId>
+                                <artifactId>liberty-maven-plugin</artifactId>
+                                <version>3.10.3</version>
+                            </dependency>
+                        </dependencies>
+                    </project>
+                """
+            )
+          )
+        );
+    }
+}
