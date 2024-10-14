75650e550717f40aed889b9a911e5246348ba5df
Tim te Beek <tim@moderne.io>
2024-10-14 11:25:47 +0200

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test/java/org/openrewrite/maven/liberty/AddOpenLibertyPluginTest.java
+++ src/test/java/org/openrewrite/maven/liberty/AddOpenLibertyPluginTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.maven.liberty;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
@@ -30,6 +31,7 @@ class AddOpenLibertyPluginTest implements RewriteTest {
         spec.parser(JavaParser.fromJavaVersion());
     }
 
+    @DocumentExample
     @Test
     void addLibertyPlugin() {
         rewriteRun(
