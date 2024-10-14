30e26d830a05e264e0756f71a89c3f5996620081
Tim te Beek <tim@moderne.io>
2024-10-14 11:28:41 +0200

    Show that plugin is added twice in multi module project

--- src/test/java/org/openrewrite/maven/liberty/AddOpenLibertyPluginTest.java
+++ src/test/java/org/openrewrite/maven/liberty/AddOpenLibertyPluginTest.java
@@ -192,6 +192,25 @@ class AddOpenLibertyPluginTest implements RewriteTest {
                         <maven.compiler.target>21</maven.compiler.target>
                         <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
                     </properties>
+                </project>
+                """,
+              """
+                <?xml version="1.0" encoding="UTF-8" ?>
+                <project xmlns="http://maven.apache.org/POM/4.0.0"
+                         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+                    <modelVersion>4.0.0</modelVersion>
+                    <groupId>com.demo</groupId>
+                    <artifactId>app-name</artifactId>
+                    <version>1.0-SNAPSHOT</version>
+                    <modules>
+                        <module>child</module>
+                    </modules>
+                    <properties>
+                        <maven.compiler.source>21</maven.compiler.source>
+                        <maven.compiler.target>21</maven.compiler.target>
+                        <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+                    </properties>
                     <build>
                         <plugins>
                             <plugin>
@@ -244,11 +263,51 @@ class AddOpenLibertyPluginTest implements RewriteTest {
                                   <artifactId>maven-war-plugin</artifactId>
                                   <version>3.3.2</version>
                               </plugin>
-                             <plugin>
-                                 <groupId>io.openliberty.tools</groupId>
-                                 <artifactId>liberty-maven-plugin</artifactId>
-                                 <version>3.10.3</version>
-                             </plugin>
+                          </plugins>
+                      </build>
+                  </project>
+                  """,
+                """
+                  <?xml version="1.0" encoding="UTF-8" ?>
+                  <project xmlns="http://maven.apache.org/POM/4.0.0"
+                           xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                           xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+                      <modelVersion>4.0.0</modelVersion>
+                      <parent>
+                          <groupId>com.demo</groupId>
+                          <artifactId>app-name</artifactId>
+                          <version>1.0-SNAPSHOT</version>
+                      </parent>
+                      <packaging>war</packaging>
+                      <modules>
+                          <module>child</module>
+                      </modules>
+                      <properties>
+                          <maven.compiler.source>21</maven.compiler.source>
+                          <maven.compiler.target>21</maven.compiler.target>
+                          <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+                      </properties>
+                      <dependencies>
+                          <dependency>
+                              <groupId>org.eclipse.microprofile</groupId>
+                              <artifactId>microprofile</artifactId>
+                              <version>6.1</version>
+                              <type>pom</type>
+                              <scope>provided</scope>
+                          </dependency>
+                      </dependencies>
+                      <build>
+                          <plugins>
+                              <plugin>
+                                  <groupId>org.apache.maven.plugins</groupId>
+                                  <artifactId>maven-war-plugin</artifactId>
+                                  <version>3.3.2</version>
+                              </plugin>
+                              <plugin>
+                                  <groupId>io.openliberty.tools</groupId>
+                                  <artifactId>liberty-maven-plugin</artifactId>
+                                  <version>3.10.3</version>
+                              </plugin>
                           </plugins>
                       </build>
                   </project>
