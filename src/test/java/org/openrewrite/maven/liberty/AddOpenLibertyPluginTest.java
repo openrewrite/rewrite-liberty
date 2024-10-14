4731ccece171077b65b7904a8dfb258bb71f75f4
Tim te Beek <tim@moderne.io>
2024-10-14 11:15:16 +0200

    Remove Java classes; demonstrate addition in multi module projects

--- src/test/java/org/openrewrite/maven/liberty/AddOpenLibertyPluginTest.java
+++ src/test/java/org/openrewrite/maven/liberty/AddOpenLibertyPluginTest.java
@@ -15,27 +15,15 @@
  */
 package org.openrewrite.maven.liberty;
 
-import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
-import static org.openrewrite.java.Assertions.*;
+import static org.openrewrite.java.Assertions.mavenProject;
 import static org.openrewrite.maven.Assertions.pomXml;
 
 class AddOpenLibertyPluginTest implements RewriteTest {
-    @Language("java")
-    private final String sampleClass = """
-         package com.test;
-
-         public class Test {         
-              public static void main(String[] args)
-              {
-                  System.out.println("test");
-               }
-         }     
-      """;
 
     @Override
     public void defaults(RecipeSpec spec) {
@@ -46,11 +34,144 @@ class AddOpenLibertyPluginTest implements RewriteTest {
     void addLibertyPlugin() {
         rewriteRun(
           spec -> spec.recipeFromResources("org.openrewrite.maven.liberty.AddOpenLibertyPlugin"),
+          //language=XML
+          pomXml(
+            """
+              <?xml version="1.0" encoding="UTF-8" ?>
+              <project xmlns="http://maven.apache.org/POM/4.0.0"
+                       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                       xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+                  <modelVersion>4.0.0</modelVersion>
+                  <groupId>com.demo</groupId>
+                  <artifactId>app-name</artifactId>
+                  <version>1.0-SNAPSHOT</version>
+                  <packaging>war</packaging>
+                  <properties>
+                      <maven.compiler.source>21</maven.compiler.source>
+                      <maven.compiler.target>21</maven.compiler.target>
+                      <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+                  </properties>
+                  <dependencies>
+                      <dependency>
+                          <groupId>org.eclipse.microprofile</groupId>
+                          <artifactId>microprofile</artifactId>
+                          <version>6.1</version>
+                          <type>pom</type>
+                          <scope>provided</scope>
+                      </dependency>
+                  </dependencies>
+                  <build>
+                      <plugins>
+                          <plugin>
+                              <groupId>org.apache.maven.plugins</groupId>
+                              <artifactId>maven-war-plugin</artifactId>
+                              <version>3.3.2</version>
+                          </plugin>
+                      </plugins>
+                  </build>
+              </project>
+              """,
+            """
+              <?xml version="1.0" encoding="UTF-8" ?>
+              <project xmlns="http://maven.apache.org/POM/4.0.0"
+                       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                       xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+                  <modelVersion>4.0.0</modelVersion>
+                  <groupId>com.demo</groupId>
+                  <artifactId>app-name</artifactId>
+                  <version>1.0-SNAPSHOT</version>
+                  <packaging>war</packaging>
+                  <properties>
+                      <maven.compiler.source>21</maven.compiler.source>
+                      <maven.compiler.target>21</maven.compiler.target>
+                      <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+                  </properties>
+                  <dependencies>
+                      <dependency>
+                          <groupId>org.eclipse.microprofile</groupId>
+                          <artifactId>microprofile</artifactId>
+                          <version>6.1</version>
+                          <type>pom</type>
+                          <scope>provided</scope>
+                      </dependency>
+                  </dependencies>
+                  <build>
+                      <plugins>
+                          <plugin>
+                              <groupId>org.apache.maven.plugins</groupId>
+                              <artifactId>maven-war-plugin</artifactId>
+                              <version>3.3.2</version>
+                          </plugin>
+                          <plugin>
+                              <groupId>io.openliberty.tools</groupId>
+                              <artifactId>liberty-maven-plugin</artifactId>
+                              <version>3.10.3</version>
+                          </plugin>
+                      </plugins>
+                  </build>
+              </project>
+              """
+          )
+        );
+    }
+
+    @Test
+    void existingLibertyPlugin() {
+        rewriteRun(
+          spec -> spec.recipeFromResources("org.openrewrite.maven.liberty.AddOpenLibertyPlugin"),
+          //language=XML
+          pomXml(
+            """
+              <?xml version="1.0" encoding="UTF-8" ?>
+              <project xmlns="http://maven.apache.org/POM/4.0.0"
+                       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                       xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+                  <modelVersion>4.0.0</modelVersion>
+                  <groupId>com.demo</groupId>
+                  <artifactId>app-name</artifactId>
+                  <version>1.0-SNAPSHOT</version>
+                  <packaging>war</packaging>
+                  <properties>
+                      <maven.compiler.source>21</maven.compiler.source>
+                      <maven.compiler.target>21</maven.compiler.target>
+                      <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+                  </properties>
+                  <dependencies>
+                      <dependency>
+                          <groupId>org.eclipse.microprofile</groupId>
+                          <artifactId>microprofile</artifactId>
+                          <version>6.1</version>
+                          <type>pom</type>
+                          <scope>provided</scope>
+                      </dependency>
+                  </dependencies>
+                  <build>
+                      <plugins>
+                          <plugin>
+                              <groupId>org.apache.maven.plugins</groupId>
+                              <artifactId>maven-war-plugin</artifactId>
+                              <version>3.3.2</version>
+                          </plugin>
+                          <plugin>
+                              <groupId>io.openliberty.tools</groupId>
+                              <artifactId>liberty-maven-plugin</artifactId>
+                              <version>3.10.3</version>
+                          </plugin>
+                      </plugins>
+                  </build>
+              </project>
+              """
+          )
+        );
+    }
+
+    @Test
+    void multiModuleProject() {
+        rewriteRun(
+          spec -> spec.recipeFromResources("org.openrewrite.maven.liberty.AddOpenLibertyPlugin"),
           mavenProject(
-            "project",
-            srcMainJava(
-              java(sampleClass)
-            ),
+            "parent",
+            //language=XML
             pomXml(
               """
                 <?xml version="1.0" encoding="UTF-8" ?>
@@ -61,63 +182,16 @@ class AddOpenLibertyPluginTest implements RewriteTest {
                     <groupId>com.demo</groupId>
                     <artifactId>app-name</artifactId>
                     <version>1.0-SNAPSHOT</version>
-                    <packaging>war</packaging>
+                    <modules>
+                        <module>child</module>
+                    </modules>
                     <properties>
                         <maven.compiler.source>21</maven.compiler.source>
                         <maven.compiler.target>21</maven.compiler.target>
                         <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
                     </properties>
-                    <dependencies>
-                        <dependency>
-                            <groupId>org.eclipse.microprofile</groupId>
-                            <artifactId>microprofile</artifactId>
-                            <version>6.1</version>
-                            <type>pom</type>
-                            <scope>provided</scope>
-                        </dependency>
-                    </dependencies>
                     <build>
                         <plugins>
-                            <plugin>
-                                <groupId>org.apache.maven.plugins</groupId>
-                                <artifactId>maven-war-plugin</artifactId>
-                                <version>3.3.2</version>
-                            </plugin>
-                        </plugins>
-                    </build>
-                </project>
-                """,
-              """
-                <?xml version="1.0" encoding="UTF-8" ?>
-                <project xmlns="http://maven.apache.org/POM/4.0.0"
-                         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-                    <modelVersion>4.0.0</modelVersion>
-                    <groupId>com.demo</groupId>
-                    <artifactId>app-name</artifactId>
-                    <version>1.0-SNAPSHOT</version>
-                    <packaging>war</packaging>
-                    <properties>
-                        <maven.compiler.source>21</maven.compiler.source>
-                        <maven.compiler.target>21</maven.compiler.target>
-                        <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
-                    </properties>
-                    <dependencies>
-                        <dependency>
-                            <groupId>org.eclipse.microprofile</groupId>
-                            <artifactId>microprofile</artifactId>
-                            <version>6.1</version>
-                            <type>pom</type>
-                            <scope>provided</scope>
-                        </dependency>
-                    </dependencies>
-                    <build>
-                        <plugins>
-                            <plugin>
-                                <groupId>org.apache.maven.plugins</groupId>
-                                <artifactId>maven-war-plugin</artifactId>
-                                <version>3.3.2</version>
-                            </plugin>
                             <plugin>
                                 <groupId>io.openliberty.tools</groupId>
                                 <artifactId>liberty-maven-plugin</artifactId>
@@ -127,60 +201,57 @@ class AddOpenLibertyPluginTest implements RewriteTest {
                     </build>
                 </project>
                 """
-            )
-          )
-        );
-    }
-    @Test
-    void existingLibertyPlugin() {
-        rewriteRun(
-          spec -> spec.recipeFromResources("org.openrewrite.maven.liberty.AddOpenLibertyPlugin"),
-          mavenProject(
-            "project",
-            srcMainJava(
-              java(sampleClass)
             ),
-            pomXml(
-                   """
-                   <?xml version="1.0" encoding="UTF-8" ?>
-                   <project xmlns="http://maven.apache.org/POM/4.0.0"
-                             xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                             xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-                        <modelVersion>4.0.0</modelVersion>
-                        <groupId>com.demo</groupId>
-                        <artifactId>app-name</artifactId>
-                        <version>1.0-SNAPSHOT</version>
-                        <packaging>war</packaging>
-                        <properties>
-                            <maven.compiler.source>21</maven.compiler.source>
-                            <maven.compiler.target>21</maven.compiler.target>
-                            <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
-                        </properties>
-                        <dependencies>
-                            <dependency>
-                                <groupId>org.eclipse.microprofile</groupId>
-                                <artifactId>microprofile</artifactId>
-                                <version>6.1</version>
-                                <type>pom</type>
-                                <scope>provided</scope>
-                            </dependency>
-                        </dependencies>
-                        <build>
-                            <plugins>
-                                <plugin>
-                                    <groupId>org.apache.maven.plugins</groupId>
-                                    <artifactId>maven-war-plugin</artifactId>
-                                    <version>3.3.2</version>
-                                </plugin>
-                                <plugin>
-                                    <groupId>io.openliberty.tools</groupId>
-                                    <artifactId>liberty-maven-plugin</artifactId>
-                                    <version>3.10.3</version>
-                                </plugin>
-                            </plugins>
-                        </build>
-                   </project>
-                   """
+            mavenProject(
+              "child",
+              //language=XML
+              pomXml(
+                """
+                  <?xml version="1.0" encoding="UTF-8" ?>
+                  <project xmlns="http://maven.apache.org/POM/4.0.0"
+                           xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                           xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+                      <modelVersion>4.0.0</modelVersion>
+                      <parent>
+                          <groupId>com.demo</groupId>
+                          <artifactId>app-name</artifactId>
+                          <version>1.0-SNAPSHOT</version>
+                      </parent>
+                      <packaging>war</packaging>
+                      <modules>
+                          <module>child</module>
+                      </modules>
+                      <properties>
+                          <maven.compiler.source>21</maven.compiler.source>
+                          <maven.compiler.target>21</maven.compiler.target>
+                          <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+                      </properties>
+                      <dependencies>
+                          <dependency>
+                              <groupId>org.eclipse.microprofile</groupId>
+                              <artifactId>microprofile</artifactId>
+                              <version>6.1</version>
+                              <type>pom</type>
+                              <scope>provided</scope>
+                          </dependency>
+                      </dependencies>
+                      <build>
+                          <plugins>
+                              <plugin>
+                                  <groupId>org.apache.maven.plugins</groupId>
+                                  <artifactId>maven-war-plugin</artifactId>
+                                  <version>3.3.2</version>
+                              </plugin>
+                             <plugin>
+                                 <groupId>io.openliberty.tools</groupId>
+                                 <artifactId>liberty-maven-plugin</artifactId>
+                                 <version>3.10.3</version>
+                             </plugin>
+                          </plugins>
+                      </build>
+                  </project>
+                  """
+              )
             )
           )
         );
