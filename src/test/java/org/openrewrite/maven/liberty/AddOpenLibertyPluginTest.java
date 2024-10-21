304bec3b03a34c481f19986af1f869f5be9aae1e
Tim te Beek <tim@moderne.io>
2024-10-21 20:04:59 +0200

    No longer expect plugin to be added to subproject
    Following https://github.com/openrewrite/rewrite/pull/4583

--- src/test/java/org/openrewrite/maven/liberty/AddOpenLibertyPluginTest.java
+++ src/test/java/org/openrewrite/maven/liberty/AddOpenLibertyPluginTest.java
@@ -266,51 +266,6 @@ class AddOpenLibertyPluginTest implements RewriteTest {
                           </plugins>
                       </build>
                   </project>
-                  """,
-                """
-                  <?xml version="1.0" encoding="UTF-8" ?>
-                  <project xmlns="http://maven.apache.org/POM/4.0.0"
-                           xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                           xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-                      <modelVersion>4.0.0</modelVersion>
-                      <parent>
-                          <groupId>com.demo</groupId>
-                          <artifactId>app-name</artifactId>
-                          <version>1.0-SNAPSHOT</version>
-                      </parent>
-                      <packaging>war</packaging>
-                      <modules>
-                          <module>child</module>
-                      </modules>
-                      <properties>
-                          <maven.compiler.source>21</maven.compiler.source>
-                          <maven.compiler.target>21</maven.compiler.target>
-                          <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
-                      </properties>
-                      <dependencies>
-                          <dependency>
-                              <groupId>org.eclipse.microprofile</groupId>
-                              <artifactId>microprofile</artifactId>
-                              <version>6.1</version>
-                              <type>pom</type>
-                              <scope>provided</scope>
-                          </dependency>
-                      </dependencies>
-                      <build>
-                          <plugins>
-                              <plugin>
-                                  <groupId>org.apache.maven.plugins</groupId>
-                                  <artifactId>maven-war-plugin</artifactId>
-                                  <version>3.3.2</version>
-                              </plugin>
-                              <plugin>
-                                  <groupId>io.openliberty.tools</groupId>
-                                  <artifactId>liberty-maven-plugin</artifactId>
-                                  <version>3.10.3</version>
-                              </plugin>
-                          </plugins>
-                      </build>
-                  </project>
                   """
               )
             )
