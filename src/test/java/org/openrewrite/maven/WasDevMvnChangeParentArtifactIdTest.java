2feee5a8217dd50014d38379899eed43dad14fb7
Tim te Beek <tim@moderne.io>
2025-06-04 23:49:03 +0200

    Drop unnecessary `JavaParser.fromJavaVersion()`

--- src/test/java/org/openrewrite/maven/WasDevMvnChangeParentArtifactIdTest.java
+++ src/test/java/org/openrewrite/maven/WasDevMvnChangeParentArtifactIdTest.java
@@ -17,18 +17,11 @@ package org.openrewrite.maven;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
-import org.openrewrite.java.JavaParser;
-import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.maven.Assertions.pomXml;
 
 class WasDevMvnChangeParentArtifactIdTest implements RewriteTest {
-
-    @Override
-    public void defaults(RecipeSpec spec) {
-        spec.parser(JavaParser.fromJavaVersion());
-    }
 
     @DocumentExample
     @Test
