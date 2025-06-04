1aab02afeae35b1318878e5faa83a9da93806c06
Tim te Beek <tim@moderne.io>
2025-06-04 23:48:34 +0200

    Drop unnecessary `JavaParser.fromJavaVersion()`

--- src/test/java/org/openrewrite/maven/liberty/AddOpenLibertyPluginTest.java
+++ src/test/java/org/openrewrite/maven/liberty/AddOpenLibertyPluginTest.java
@@ -17,19 +17,12 @@ package org.openrewrite.maven.liberty;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
-import org.openrewrite.java.JavaParser;
-import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.mavenProject;
 import static org.openrewrite.maven.Assertions.pomXml;
 
 class AddOpenLibertyPluginTest implements RewriteTest {
-
-    @Override
-    public void defaults(RecipeSpec spec) {
-        spec.parser(JavaParser.fromJavaVersion());
-    }
 
     @DocumentExample
     @Test
