b60adf86a0a6ccc4854e6cc52b059762f7e195a4
Tim te Beek <timtebeek@gmail.com>
2025-06-27 17:43:43 +0000

    Pin JUnit to 5.+
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.text.FindAndReplace?organizationId=ODQ2MGExMTUtNDg0My00N2EwLTgzMGMtNGE1NGExMTBmZDkw#defaults%3DW3sidmFsdWUiOiI6anVuaXQtKC4qKTpsYXRlc3QucmVsZWFzZSIsIm5hbWUiOiJmaW5kIn0seyJ2YWx1ZSI6IjpqdW5pdC0kMTo1LisiLCJuYW1lIjoicmVwbGFjZSJ9LHsidmFsdWUiOiJUcnVlIiwibmFtZSI6InJlZ2V4In0seyJ2YWx1ZSI6ImJ1aWxkLmdyYWRsZS5rdHMiLCJuYW1lIjoiZmlsZVBhdHRlcm4ifV0%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,5 +19,5 @@ dependencies {
     testImplementation("org.openrewrite:rewrite-gradle")
     testImplementation("org.openrewrite:rewrite-maven")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:latest.release")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.+")
 }
