e9bb78de49eb320e4a8f0ac6e469218983630272
Tim te Beek <tim@moderne.io>
2025-08-10 13:42:44 +0200

    Add missing import

--- src/test/java/org/openrewrite/maven/liberty/AddOpenLibertyPluginTest.java
+++ src/test/java/org/openrewrite/maven/liberty/AddOpenLibertyPluginTest.java
@@ -17,6 +17,7 @@ package org.openrewrite.maven.liberty;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
+import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.mavenProject;
@@ -24,12 +25,10 @@ import static org.openrewrite.maven.Assertions.pomXml;
 
 class AddOpenLibertyPluginTest implements RewriteTest {
 
-
     @Override
     public void defaults(RecipeSpec spec) {
         spec.recipeFromResources( "org.openrewrite.maven.liberty.AddOpenLibertyPlugin" );
     }
-
 
     @DocumentExample
     @Test
