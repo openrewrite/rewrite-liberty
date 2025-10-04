45c89a98aa55fa94a74ab3f8c5e5ca8190bb1a60
Tim te Beek <tim@moderne.io>
2025-10-04 13:18:50 +0200

    Update recipe documentation examples (#37)
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.recipes.rewrite.OpenRewriteRecipeBestPractices?organizationId=ODQ2MGExMTUtNDg0My00N2EwLTgzMGMtNGE1NGExMTBmZDkw
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/java/org/openrewrite/java/liberty/ServerNameTest.java
+++ src/test/java/org/openrewrite/java/liberty/ServerNameTest.java
@@ -17,6 +17,7 @@
 package org.openrewrite.java.liberty;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
@@ -62,6 +63,7 @@ class ServerNameTest implements RewriteTest {
       }
       """;
 
+    @DocumentExample
     @Test
     void replaceGetFullNameTest() {
         rewriteRun(
